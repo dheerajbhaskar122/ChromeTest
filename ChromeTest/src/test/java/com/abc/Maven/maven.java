effa088ad9aa8089d6f09ea63860068d098da2f2
HP <HP@DESKTOP-LEVELMS>
2019-04-04 10:28:53 +0530

    commit1

---
+++ ChromeTest/src/test/java/com/abc/Maven/maven.java
@@ -0,0 +1,13 @@
+package com.abc.Maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class maven {
+@Test
+public void chrometest()
+{
+	WebDriver driver=new ChromeDriver();
+}
+}
